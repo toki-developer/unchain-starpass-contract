3fd5e3b9d4987dcfd240422ca4bfdbe9ad5f9328
Toki <55911995+tokio-k@users.noreply.github.com>
2023-06-29 23:38:09 +0900

    コメント追加

--- contracts/SocialNetwork.sol
+++ contracts/SocialNetwork.sol
@@ -5,9 +5,11 @@ pragma solidity ^0.8.17;
 import "./interfaces/ISocialNetwork.sol";
 
 contract SocialNetwork is ISocialNetwork {
+    // 投稿一覧 postIdは連番
     Post[] private _posts;
     mapping(uint256 => address[]) private _likers;
 
+    // Postの情報に、postId、いいねの数、いいね済みかどうかの情報を追加
     function _getPostDetail(uint256 _postId)
         private
         view
@@ -34,6 +36,7 @@ contract SocialNetwork is ISocialNetwork {
             });
     }
 
+    //投稿
     function post(string memory _message) external {
         Post memory newPost = Post({
             author: msg.sender,
@@ -43,6 +46,7 @@ contract SocialNetwork is ISocialNetwork {
         _posts.push(newPost);
     }
 
+    //最後の投稿のIDを取得
     function getLastPostId() external view returns (uint256) {
         if (_posts.length == 0) {
             revert("Posts does not exist.");
@@ -50,6 +54,7 @@ contract SocialNetwork is ISocialNetwork {
         return _posts.length - 1;
     }
 
+    //投稿を1件取得
     function getPost(uint256 _postId)
         external
         view
@@ -58,7 +63,9 @@ contract SocialNetwork is ISocialNetwork {
         return _getPostDetail(_postId);
     }
 
+    //投稿にいいね
     function like(uint256 _postId) external {
+        //すでにいいねの場合はエラー
         for (uint256 i = 0; i < _likers[_postId].length; i++) {
             if (_likers[_postId][i] == msg.sender) {
                 revert("Already liked");
@@ -68,25 +75,30 @@ contract SocialNetwork is ISocialNetwork {
         _likers[_postId].push(msg.sender);
     }
 
+    //投稿からいいねの削除
     function unlike(uint256 _postId) external {
         uint256 targetIndex = type(uint256).max;
 
+        //いいねを取り消す対象を探す
         for (uint256 i = 0; i < _likers[_postId].length; i++) {
             if (_likers[_postId][i] == msg.sender) {
                 targetIndex = i;
             }
         }
 
+        //対象をいいねしていない場合はエラー
         if (targetIndex == type(uint256).max) {
             revert("Not already liked");
         }
 
+        //いいねのデータを1つずつ前に詰める。上書きすることでいいねを削除
         for (uint256 i = targetIndex; i < _likers[_postId].length - 1; i++) {
             _likers[_postId][i] = _likers[_postId][i + 1];
         }
         _likers[_postId].pop();
     }
 
+    //いいねを複数権取得
     // order => 1: time(asc), 2:time(desc), 3:like(asc), 4:like(desc)
     function getPosts(uint256 order)
         external
@@ -102,7 +114,7 @@ contract SocialNetwork is ISocialNetwork {
         for (uint256 i = 0; i < _posts.length; i++) {
             PostDetail memory postDetail = _getPostDetail(i);
 
-            //Put data into RESULT while sorting
+            //ソートしながら、resultにデータを入れる
             if (order == 1) {
                 result[i] = postDetail;
             } else if (order == 2) {
